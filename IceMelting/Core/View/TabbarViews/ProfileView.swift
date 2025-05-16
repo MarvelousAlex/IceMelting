506f822b91f7aa6f554ce561f8a50f254664b9d7
Wende Zhou <heywendezhou@outlook.com>
2025-05-16 16:36:27 +1000

    done

--- IceMelting/Core/View/TabbarViews/ProfileView.swift
+++ IceMelting/Core/View/TabbarViews/ProfileView.swift
@@ -30,62 +30,77 @@ struct ProfileView: View {
     private let sports = ["Badminton", "Basketball", "Tennis", "Swimming", "Baseball", "Volleyball"]
     
     var body: some View {
-        ZStack {
-            Color.skinn.ignoresSafeArea()
-            VStack(spacing: 20) {
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(Color.armyGreen)
-                    .frame(height: 100)
-                    .overlay {
+        NavigationStack {
+            ZStack {
+                Color.skinn.ignoresSafeArea()
+                VStack(spacing: 20) {
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundStyle(Color.armyGreen)
+                        .frame(height: 100)
+                        .overlay {
+                            HStack {
+                                Text("Wendy")
+                                    .foregroundStyle(.white)
+                                    .font(.custom("K2D-SemiBold", size: 30))
+                                    .frame(alignment: .leading)
+                                Spacer().frame(width: 250)
+                            }
+                        }
+                    
+                    ScrollView(.horizontal) {
                         HStack {
-                            Text("Wendy")
-                                .foregroundStyle(.white)
-                                .font(.custom("K2D-SemiBold", size: 30))
-                                .frame(alignment: .leading)
-                            Spacer().frame(width: 250)
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 50, height: 30)
+                                .foregroundStyle(Color.shineRed)
+                                .overlay {
+                                    Text("HD")
+                                }
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 125, height: 30)
+                                .foregroundStyle(Color.littleBlue)
+                                .overlay {
+                                    Text("Data Analtics")
+                                }
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 60, height: 30)
+                                .foregroundStyle(Color.armyGreen)
+                                .overlay {
+                                    Text("UI/UX")
+                                }
+                            
+                            RoundedRectangle(cornerRadius: 20)
+                                .frame(width: 110, height: 30)
+                                .foregroundStyle(Color.skinYellow)
+                                .overlay {
+                                    Text("Badminton")
+                                }
                         }
                     }
-                
-                ScrollView(.horizontal) {
-                    HStack {
-                        RoundedRectangle(cornerRadius: 20)
-                            .frame(width: 50, height: 30)
-                            .foregroundStyle(Color.shineRed)
-                            .overlay {
-                                Text("HD")
-                            }
-                        
-                        RoundedRectangle(cornerRadius: 20)
-                            .frame(width: 125, height: 30)
-                            .foregroundStyle(Color.littleBlue)
-                            .overlay {
-                                Text("Data Analtics")
-                            }
-                        
-                        RoundedRectangle(cornerRadius: 20)
-                            .frame(width: 60, height: 30)
-                            .foregroundStyle(Color.armyGreen)
-                            .overlay {
-                                Text("UI/UX")
-                            }
-                        
-                        RoundedRectangle(cornerRadius: 20)
-                            .frame(width: 110, height: 30)
-                            .foregroundStyle(Color.skinYellow)
-                            .overlay {
-                                Text("Badminton")
-                            }
-                    }
+                    
+                    IceBreakingSummaryView()
+                        .padding(.horizontal, 2)
+                    
+                    SkillExchangeView()
+                        .padding(.horizontal, 2)
                 }
-                
-                IceBreakingSummaryView()
-                    .padding(.horizontal, 2)
-                
-                SkillExchangeView()
-                    .padding(.horizontal, 2)
+                .padding()
             }
-            .padding()
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sessionStore.signOut()
+                    } label: {
+                        Text("Log Out")
+                            .foregroundStyle(.black)
+                            .font(.custom("K2D-Bold", size: 16))
+                    }
+
+                }
+            }
         }
+        
     }
 }
 
